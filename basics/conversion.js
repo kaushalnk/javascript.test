95ae7d5c5211535042f06da8470e1493621716c8
Kaushal <122157946+kaushalnk@users.noreply.github.com>
2024-08-28 10:07:05 +0000

    config

---
+++ basics/conversion.js
@@ -0,0 +1,14 @@
+let rollno ="23"
+
+console.log(typeof rollno)
+console.log(rollno)
+
+let noformate = Number(rollno)
+
+console.log(typeof noformate)
+console.log(noformate)
+//so like this we can change datatypes
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+2+"3") //precidence dependence
+    
---
+++ basics/heapstack.js
@@ -0,0 +1,12 @@
+let varone ="kaushal"
+let vartwo = varone
+
+console.log(varone)
+console.log(vartwo)
+
+vartwo = "nayak"
+
+console.log(vartwo)
+
+//primitive datatypes are stored in stack 
+//reference datatype are stored in heap every time a reference is given to variable but in stack a copy of value are given
